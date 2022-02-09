c3634164660d4796ad92478a8cc4a0f9f3f9e6d5
Reilit <90870270+Reilit-Kimberly@users.noreply.github.com>
2022-02-09 18:56:26 +0300

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int number;
+    char symbol;
+    scanf("%d %c", &number, &symbol);
+    printf("%d %c \n", number, symbol);
+    system("pause");
+    return 0;
+}
